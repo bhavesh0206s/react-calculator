3d53a6c3d2d7bd4840503c457211bf9d4521811f
bhavesh0206s <bhaveshsuthar0299@gmail.com>
2020-01-08 19:40:49 +0530

    Updates

---
+++ precache-manifest.0f5d48884a3c6d5df78a22ea96f67aa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177c20d11f6fe5ed3d0cd44e58439cc4",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "84450313d0c22f4b1123",
+    "url": "/react-calculator/static/css/main.2f0cc54b.chunk.css"
+  },
+  {
+    "revision": "17f2104a15a820b622e4",
+    "url": "/react-calculator/static/js/2.476d0f82.chunk.js"
+  },
+  {
+    "revision": "84450313d0c22f4b1123",
+    "url": "/react-calculator/static/js/main.9300d867.chunk.js"
+  },
+  {
+    "revision": "18c5e34496daf9295170",
+    "url": "/react-calculator/static/js/runtime-main.6cb08487.js"
+  }
+]);
